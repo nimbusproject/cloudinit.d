0392154cf7ba12f4ba56d5a75764dc96e5e5e550
Tim Freeman <tim@timfreeman.org>
2011-01-12 14:36:36 -0600

    Rearranged how tarballs work in fab tasks. Inline with current README.boottool.txt description now.

--- cloudboot/bootfabtasks.py
+++ cloudboot/bootfabtasks.py
@@ -1,35 +1,70 @@
 __author__ = 'bresnaha'
 
 import os
-from fabric.api import env, run, put, sudo
+from fabric.api import env, run, put, sudo, cd
+
+REMOTE_WORKING_DIR = "/tmp/nimbusready"
+
+def _iftar(filename):
+    """Return base filename if filename ends with .tar.gz (and does not simply equal that suffix)
+    Otherwise return None
+    """
+    if not filename:
+        return None
+    idx = filename.rfind('.tar.gz')
+    if idx > 0:
+        return filename[:idx]
+    else:
+        return None
+
+def _tartask(directory, basename, tarball):
+    """Expand the tarball, ensure it contains a directory with the basename.
+    Ensure run.sh exists inside.
+    Return path to the run.sh file.
+    """
+    with cd(directory):
+        run("tar -xvzf %s" % tarball)
+    tardir = os.path.join(directory, basename)
+    try:
+        run("test -d %s" % tardir)
+    except:
+        raise Exception("The tarball does not expand to a directory of the same name: %s" % tardir)
+    destpgm = os.path.join(tardir, "run.sh")
+    try:
+        run("test -f %s" % destpgm)
+    except:
+        raise Exception("The tarball does contain a 'run.sh' file: %s" % tarball)
+
+    # In case they forgot:
+    run("chmod +x %s" % destpgm)
+    
+    return destpgm
 
 def readypgm(pgm=None):
-    readydir = '/tmp/nimbusready'
     env.warn_only = True
-    run('mkdir %s' % (readydir))
+    run('mkdir %s' % REMOTE_WORKING_DIR)
     relpgm = os.path.basename(pgm)
-    destpgm = "%s/%s" % (readydir, relpgm)
+    destpgm = "%s/%s" % (REMOTE_WORKING_DIR, relpgm)
     put(pgm, destpgm, mode=0755)
-    if relpgm.find('tar.gz') >= 0:
-        run("tar -zvf %s" % (destpgm))
-        destpgm = '%s/run.sh' % (readydir)    
-    cmd = "cd %s; %s" % (readydir, destpgm)
+    tarname = _iftar(relpgm)
+    if tarname:
+         destpgm = _tartask(REMOTE_WORKING_DIR, tarname, destpgm)
     env.warn_only = False
-    sudo(cmd)
+    with cd(REMOTE_WORKING_DIR):
+        sudo(destpgm)
 
 def bootpgm(pgm=None, conf=None):
-    confdir = '/tmp/nimbusconf'
-    run('mkdir %s' % (confdir))
+    run('mkdir %s' % REMOTE_WORKING_DIR)
     relpgm = os.path.basename(pgm)
-    destpgm = "%s/%s" % (confdir, relpgm)
+    destpgm = "%s/%s" % (REMOTE_WORKING_DIR, relpgm)
     put(pgm, destpgm, mode=0755)
-    if relpgm.find('tar.gz') >= 0:
-        run("tar -zvf %s" % (destpgm))
-        destpgm = '%s/run.sh' % (confdir)
+    tarname = _iftar(relpgm)
+    if tarname:
+        destpgm = _tartask(REMOTE_WORKING_DIR, tarname, destpgm)
     if conf:
-        destconf = "%s/bootconf.json" % (confdir)
+        destconf = "%s/bootconf.json" % REMOTE_WORKING_DIR
         put(conf, destconf)
-    cmd = "cd %s; %s" % (confdir, destpgm)
-    sudo(cmd)
+    with cd(REMOTE_WORKING_DIR):
+        sudo(destpgm)
 
 
