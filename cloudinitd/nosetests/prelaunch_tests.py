23e014f615244e51d398802669b55c9bc1391a54
John Bresnahan <bresnaha@mcs.anl.gov>
2011-03-18 17:51:15 -0500

    minor patch to test to work with non ec2 hostname

--- cloudinitd/nosetests/prelaunch_tests.py
+++ cloudinitd/nosetests/prelaunch_tests.py
@@ -22,11 +22,13 @@ class BasicUserAPITests(unittest.TestCase):
         try:
             key = os.environ['CLOUDBOOT_IAAS_ACCESS_KEY']
             secret = os.environ['CLOUDBOOT_IAAS_SECRET_KEY']
+            host = os.environ['CLOUDBOOT_IAAS_HOSTNAME']
+            port = os.environ['CLOUDBOOT_IAAS_PORT']
         except:
             pass
 
         # XXX this test may fail for nimbus
-        con = cloudinitd.cb_iaas.iaas_get_con(None, key=key, secret=secret)
+        con = cloudinitd.cb_iaas.iaas_get_con(None, key=key, secret=secret, iaashostname=host, iaasport=port)
         i_list = con.get_all_instances()
         conf_file = "multilevelsimple"
         self.plan_basedir = cloudinitd.nosetests.g_plans_dir
