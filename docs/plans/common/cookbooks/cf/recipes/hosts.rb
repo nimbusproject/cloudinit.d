f09dcab4b9f953a58953d8b4dfc5df0db43e1bd7
Patrick Armstrong <patricka@uvic.ca>
2011-08-02 14:01:16 -0700

    Add simple multinode cf launch plan.
    Has one node with everything but DEA, and one DEA node

---
+++ docs/plans/common/cookbooks/cf/recipes/hosts.rb
@@ -0,0 +1,18 @@
+require 'resolv'
+
+api_host = node[:api_host]
+controller_host = node[:controller_host]
+controller_ip = Resolv.new.getaddress(controller_host)
+
+
+script "Setup /etc/hosts" do
+  interpreter "bash"
+  user "root"
+  environment ({'IP' => controller_ip, 'API' => api_host})
+  not_if "grep vcap /etc/hosts"
+  code <<-EOH
+  echo $IP $API >> /etc/hosts
+  echo '#Add app hostnames here:' >> /etc/hosts
+  echo "#\$IP testapp.vcap" >> /etc/hosts
+  EOH
+end
