2c5647f20c4daf42b60c65a77991c51f9a2f17ee
Patrick Armstrong <patricka@uvic.ca>
2011-08-03 22:32:52 -0700

    John's tests now working on the multinode cf plan.

--- docs/plans/cloudfoundry/01/readytests/run_app.sh
+++ docs/plans/cloudfoundry/01/readytests/run_app.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+source bootenv.sh
+
+if [ -z "$vcap_domain" ] ; then
+    vcap_domain="vcap.me"
+fi
+
+if [ -z "$api_host" ] ; then
+    api_host="api.$vcap_domain"
+fi
+
 cd $1 
+vmc target $api_host
 vmc register --email foo@bar.com --passwd password
 vmc login --email foo@bar.com --passwd password
 
 # the above may already be there and i cannot seem to clean them up
 set -e
-vmc push env --instances 4 --mem 64M --url env.vcap.me -n
+vmc push env --instances 4 --mem 64M --url env.$vcap_domain -n
 
-curl  http://env.vcap.me | grep "Hello from the Cloud"
-vmc apps | grep 'env.vcap.me'
+curl  http://env.$vcap_domain | grep "Hello from the Cloud"
+vmc apps | grep "env.$vcap_domain"
 
 vmc delete env
-
