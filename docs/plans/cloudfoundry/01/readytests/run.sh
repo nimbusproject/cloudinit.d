9bc5924e53ab9d08d26a66d5891dbb021e69d72f
Patrick Armstrong <patricka@uvic.ca>
2011-08-04 13:10:36 -0700

    Fix problem with multinode setup when domain isn't vcap.me

--- docs/plans/cloudfoundry/01/readytests/run.sh
+++ docs/plans/cloudfoundry/01/readytests/run.sh
@@ -7,6 +7,6 @@ set -e
 if [ "X$vcaptests" != "X" ]; then
     sudo -H -i -u $username `pwd`/vcap_tests.sh
 fi
-sudo -H -i -u $username `pwd`/run_app.sh `pwd`
+sudo  vcap_domain=$vcap_domain api_host=$api_host -H -i -u $username `pwd`/run_app.sh `pwd`
 ./cf_ready.sh
 
