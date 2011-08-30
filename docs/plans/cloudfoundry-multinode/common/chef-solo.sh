4f07dbb1e2bb1a97ddcb299935c3011f6f8097e7
Patrick Armstrong <patricka@uvic.ca>
2011-08-30 13:02:13 -0700

    Update multinode CF launch plan for changes to cf cookbooks

--- docs/plans/cloudfoundry-multinode/common/chef-solo.sh
+++ docs/plans/cloudfoundry-multinode/common/chef-solo.sh
@@ -7,13 +7,12 @@
 APP_NAME="Cloud Foundry"
 
 COOKBOOK_GIT_URL="https://github.com/oldpatricka/vcap"
-COOKBOOK_GIT_BRANCH="cloudinit"
-COOKBOOK_REPO_DIR="/opt/vcap"
+COOKBOOK_GIT_BRANCH="cloudinit-merge"
+COOKBOOK_REPO_DIR="/var/vcap"
 COOKBOOK_DIR="$COOKBOOK_REPO_DIR/dev_setup/cookbooks"
 ROLES_DIR="$COOKBOOK_REPO_DIR/dev_setup/roles"
 CHEF_LOGLEVEL="info"
 MAKE_DIRS="/var/vcap.local"
-IP_ADDRESS=`host $HOSTNAME`
 
 function exit_on_error_with_error {
   is_error="$1"
@@ -111,13 +110,21 @@ else
 
   $CMDPREFIX git clone $COOKBOOK_GIT_URL $COOKBOOK_REPO_DIR
   exit_on_error_with_error $? "Couldn't clone git repo '$COOKBOOK_GIT_URL'"
+
 fi
+
 
 (cd $COOKBOOK_REPO_DIR && $CMDPREFIX git checkout $COOKBOOK_GIT_BRANCH)
 exit_on_error_with_error $? "Couldn't checkout '$COOKBOOK_GIT_BRANCH' branch"
 
 (cd $COOKBOOK_REPO_DIR && $CMDPREFIX git pull)
 exit_on_error_with_error $? "Couldn't pull '$COOKBOOK_GIT_URL'"
+
+(cd $COOKBOOK_REPO_DIR && $CMDPREFIX git submodule update --init)
+exit_on_error_with_error $? "Couldn't update submodules for '$COOKBOOK_GIT_URL'"
+
+$CMDPREFIX chown -R $username $COOKBOOK_REPO_DIR
+exit_on_error_with_error $? "Couldn't chown '$COOKBOOK_REPO_DIR'"
 
 echo "Retrieved Chef Cookbook from '$COOKBOOK_GIT_URL'"
 
@@ -188,13 +195,12 @@ $CMDPREFIX mv rerun-chef-$RUN_NAME.sh /opt/rerun-chef-$RUN_NAME.sh
 
 echo "Running chef-solo"
 $CMDPREFIX /opt/rerun-chef-$RUN_NAME.sh
-
 #workaround to ensure new password is picked up
 echo "Kill nats"
 ps aux | awk '/nats-server/ && !/awk/ {print $2}' | xargs $CMDPREFIX kill
 
 echo "Start vcap"
-echo . $vcap_profile
-echo $vcap_home/bin/vcap -c $vcap_config start $vcap_start
 . $vcap_profile
+# Kill nats server in case there's a config change we want to take effect
+$CMDPREFIX kill `ps aux | grep nats-server | grep -v grep |awk '{print $2}'`
 $vcap_home/bin/vcap -c $vcap_config start $vcap_start
