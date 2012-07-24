1657b31cb3410a73fb7d0fc6189abd076751080d
BuzzTroll <buzztroll@gmail.com>
2012-07-24 16:06:30 -0500

    merged in local services branch.  Fake mode tests passing

--- tests/plans/localhost/bootpgm.sh
+++ tests/plans/localhost/bootpgm.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-if [ ! -e bootconf.json ]; then
-    echo "The boot conf file does not exist!"
-    pwd
-    exit 1
-fi
-if [ ! -e bootenv.sh ]; then
-    echo "The boot conf file does not exist!"
-    pwd
-    exit 1
-fi
-
 exit 0
